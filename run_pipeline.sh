098cc696ab16d69902644a4bcaa0756d89e1d84c
Bethany Suter <bsuter@caltech.edu>
2017-05-06 00:17:48 -0700

    first commit of the makefile

---
+++ run_pipeline.sh
@@ -0,0 +1,23 @@
+# Generates all the plots for the article
+
+ipython lab3.py 'explicit' 'dist'
+
+ipython lab3.py 'explicit' 'vel'
+
+ipython lab3.py 'error'
+
+ipython lab3.py 'truncation'
+
+ipython lab3.py 'energy'
+
+ipython lab3.py 'implicit' 'error'
+
+ipython lab3.py 'implicit' 'energy'
+
+ipython lab3.py 'phaseSpace' 'dist'
+
+ipython lab3.py 'phaseSpace' 'vel'
+
+ipython lab3.py 'symplectic'
+
+ipython lab3.py 'sympEnergy'
